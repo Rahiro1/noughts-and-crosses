d7ce389c62bc70f6cf24742a76b2ff0f671aceae
James <jdt130@bham.ac.uk>
2025-04-29 18:39:32 +0100

    Add functionto check for win

--- scripts.js
+++ scripts.js
@@ -5,7 +5,45 @@ const gameManager = (function(){
     let playerOne = createPlayer("X");
     let playerTwo = createPlayer("O");
     let activePlayer = playerOne;
-    const hasWon = () => "Not Implemented Error";
+
+    const gameboard = (function(){
+        let gameSquares = [createGameSquare(), createGameSquare(), createGameSquare(), createGameSquare(), createGameSquare(), createGameSquare(), createGameSquare(), createGameSquare(), createGameSquare()]
+
+        return {gameSquares};
+
+        function createGameSquare(){
+            symbol : "";
+            return {symbol}; 
+        }
+    });
+
+
+    function checkForWin () {
+        let board = gameboard.gameSquares;
+        let isGameEnd = false;
+        let winner = ""; 
+
+        checkThreeSquares(0,1,2);
+        checkThreeSquares(0,3,6);
+        checkThreeSquares(0,4,8);
+        checkThreeSquares(2,5,8);
+        checkThreeSquares(2,4,7);
+        checkThreeSquares(6,7,8);
+
+        return {isGameEnd, winner};
+
+        // checks if all three squares have the same symbol and flags the game to end if so
+        function checkThreeSquares(sqOne, sqTwo, sqThree){
+            if(board[sqOne].symbol === board[sqTwo].symbol && board[sqOne].symbol === board[sqThree].symbol){
+                flagGameEnd(board[sqOne].symbol);
+            }
+        }
+
+        function flagGameEnd(winSymbol){
+            isGameEnd = true;
+            winner = winSymbol;
+        }
+    };
 
     const getActivePlayer = () => activePlayer;
     const setActivePlayer = (player) => activePlayer = player;
@@ -23,14 +61,5 @@ const gameManager = (function(){
         return {symbol};
     }
 
-    function createGameBoard(){
-        let gameSquares = [createGameSquare(), createGameSquare(), createGameSquare(), createGameSquare(), createGameSquare(), createGameSquare(), createGameSquare(), createGameSquare(), createGameSquare()]
-
-        return {gameSquares};
-
-        function createGameSquare(){
-            symbol : "";
-            return {symbol}; 
-        }
-    }
+    
 });
